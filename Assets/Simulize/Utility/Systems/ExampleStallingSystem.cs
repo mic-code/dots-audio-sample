5603acb2b6cdfeccd1e45704e04e59c3fca3ddb2
Clint Valusek <clint@simulize.net>
2020-05-13 23:46:39 -0500

    Adding in a system to cause stalls to simulate load.

---
+++ Assets/Simulize/Utility/Systems/ExampleStallingSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Simulize.Utility
+{
+    [AlwaysUpdateSystem]
+    public class ExampleStallingSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.BackQuote))
+            {
+                Thread.Sleep(1);
+            }
+
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                Thread.Sleep(10);
+            }
+
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                Thread.Sleep(100);
+            }
+
+            if (Input.GetKey(KeyCode.Alpha3))
+            {
+                Thread.Sleep(200);
+            }
+
+            if (Input.GetKey(KeyCode.Alpha4))
+            {
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
